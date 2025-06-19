bf95627d552551dac5648aeca813c479a7cb412d
Gabriel Cavalante <132387174+GabsCavalcant@users.noreply.github.com>
2025-06-19 01:58:51 -0300

    ExampleCompareToo

---
+++ compareToo/src/App.java
@@ -0,0 +1,11 @@
+
+public class App  {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Maria".compareTo("Alex"));
+        System.out.println("Alex".compareTo("Maria"));
+        System.out.println("Alex".compareTo("Alex"));
+    }
+
+
+}
